dba0c2d46509cf2c7d0d624b02ba74393b9a2474
eunji-222 <enjijo612@gmail.com>
2026-08-16 23:43:02 +0900

    TourType에 한글 이름 필드 추가

--- src/main/java/com/tourbooking/model/TourType.java
+++ src/main/java/com/tourbooking/model/TourType.java
@@ -2,10 +2,20 @@ package com.tourbooking.model;
 
 public enum TourType {
 
-    CITY ("시내"),
-    HERITAGE ("유적지"),
-    NATURE ("자연 경관"),
-    ACTIVITY ("체험")
+    CITY ("시내관광"),      //기항지의 시내 명소 관광 코스
+    HERITAGE ("유적지"),   //유네스코 문화유산 방문 코스
+    NATURE ("자연경관"),   //유네스코 자연유산 방문 코스
+    ACTIVITY ("체험");       //활동, 체험, 교류 코스
 
+
+    private final String typeName;
+
+    TourType (String typeName) {
+        this.typeName = typeName;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
 
 }
