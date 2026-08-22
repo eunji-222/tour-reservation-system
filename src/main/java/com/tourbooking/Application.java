fe5fa852aa82ef93e29d986d60fa13fb61679538
eunji-222 <enjijo612@gmail.com>
2026-08-23 00:28:33 +0900

    while 반복 메뉴 구현

--- src/main/java/com/tourbooking/Application.java
+++ src/main/java/com/tourbooking/Application.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("===== 기항지 투어 예약 시스템 =====");
-        System.out.println("1. 예약 등록");
-        System.out.println("2. 예약 조회");
-        System.out.println("3. 예약 수정");
-        System.out.println("4. 예약 취소");
-        System.out.println("5. 종료");
-        System.out.println();     // 빈 출력 넣어서 화면에 여백 만들었음
 
         Scanner scanner = new Scanner(System.in);
-        System.out.print("메뉴를 선택하세요: ");
-        int choice = scanner.nextInt();
-        System.out.println(choice + "번을 선택하셨습니다.");
-    }
+        while(true) {
+            System.out.println("===== 기항지 투어 예약 시스템 =====");
+            System.out.println("1. 예약 등록");
+            System.out.println("2. 예약 조회");
+            System.out.println("3. 예약 수정");
+            System.out.println("4. 예약 취소");
+            System.out.println("5. 종료");
+            System.out.println();
+            System.out.print("메뉴를 선택하세요: ");
+            int choice = scanner.nextInt();
+            System.out.println(choice + "번을 선택하셨습니다.");
+            System.out.println();
+        }
+
+        }
 
     }
+
 
 
