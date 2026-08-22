c7ab4c0e6ab313e59ff43e18b7614bde3253e7c3
eunji-222 <enjijo612@gmail.com>
2026-08-23 00:06:27 +0900

    Application에 Scanner 입력 추가

--- src/main/java/com/tourbooking/Application.java
+++ src/main/java/com/tourbooking/Application.java
@@ -1,5 +1,7 @@
 package com.tourbooking;
 
+import java.util.Scanner;
+
 public class Application {
 
     public static void main(String[] args) {
@@ -9,6 +11,14 @@ public class Application {
         System.out.println("3. 예약 수정");
         System.out.println("4. 예약 취소");
         System.out.println("5. 종료");
+        System.out.println();     // 빈 출력 넣어서 화면에 여백 만들었음
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("메뉴를 선택하세요: ");
+        int choice = scanner.nextInt();
+        System.out.println(choice + "번을 선택하셨습니다.");
     }
 
-}
+    }
+
+
