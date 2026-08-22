1cf0e634e84fede51871d0c9c997dbd2aa1a1f54
eunji-222 <enjijo612@gmail.com>
2026-08-22 23:10:51 +0900

    Application 클래스 만들고 메뉴 출력 구현

---
+++ src/main/java/com/tourbooking/Application.java
@@ -0,0 +1,14 @@
+package com.tourbooking;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("===== 기항지 투어 예약 시스템 =====");
+        System.out.println("1. 예약 등록");
+        System.out.println("2. 예약 조회");
+        System.out.println("3. 예약 수정");
+        System.out.println("4. 예약 취소");
+        System.out.println("5. 종료");
+    }
+
+}
