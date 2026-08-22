43f49cfd2e2150d156bdfbe0748d0515ce9ef295
eunji-222 <enjijo612@gmail.com>
2026-08-23 00:52:18 +0900

    switch 메뉴 분기 구현

--- src/main/java/com/tourbooking/Application.java
+++ src/main/java/com/tourbooking/Application.java
@@ -11,14 +11,33 @@ public class Application {
             System.out.println("===== 기항지 투어 예약 시스템 =====");
             System.out.println("1. 예약 등록");
             System.out.println("2. 예약 조회");
-            System.out.println("3. 예약 수정");
+            System.out.println("3. 예약 변경");
             System.out.println("4. 예약 취소");
             System.out.println("5. 종료");
             System.out.println();
             System.out.print("메뉴를 선택하세요: ");
             int choice = scanner.nextInt();
-            System.out.println(choice + "번을 선택하셨습니다.");
-            System.out.println();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("[예약 등록]");
+                    break;
+                case 2:
+                    System.out.println("[예약 조회]");
+                    break;
+                case 3:
+                    System.out.println("[예약 변경]");
+                    break;
+                case 4:
+                    System.out.println("[예약 취소]");
+                    break;
+                case 5:
+                    System.out.println("프로그램을 종료합니다.");
+                    return;
+                default:
+                    System.out.println("[오류] 1~5 사이의 번호를 입력해주세요.");
+
+            }
         }
 
         }
